c6f5cc498462c43cb24036e6afa2647c4c9ddce2
Aarya_Butolia <146535399+AaryaButolia11@users.noreply.github.com>
2025-05-09 00:56:03 +0530

    Add files via upload

---
+++ basic_hello-world.cpp
@@ -0,0 +1,11 @@
+// used to include iostream ie used for i/o in cpp programs
+#include<iostream>
+
+//compiler finds first main func
+int main(){
+    // cout used to print on display
+    std::cout<<"Hello World";
+
+    //successfull completion of fnc
+    return 0;
+}

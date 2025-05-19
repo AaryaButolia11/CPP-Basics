08ca9751c50473049fbb8daa99781449754f6004
Aarya_Butolia <146535399+AaryaButolia11@users.noreply.github.com>
2025-05-19 23:57:06 +0530

    Add files via upload

---
+++ type-casting.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int a = 45;
+    float b=45.46;
+    // in "(<datatype>)" before variable write the datatype u want to change in
+    cout<<"the value of a is: "<<(float)a;
+    cout<<"the value of b in int is: "<<(int)a;
+} // namespace std
